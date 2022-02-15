e7a8c0f4b5173da69b59d7d7a6c87f94f4678190
NITHISH5464 <99641748+NITHISH5464@users.noreply.github.com>
2022-02-15 17:25:42 +0530

    Add files via upload

---
+++ pallindrom.java
@@ -0,0 +1,36 @@
+package assignment.com;
+
+import java.util.Iterator;
+
+import org.apache.poi.hssf.record.chart.SeriesToChartGroupRecord;
+
+public class pallindrom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String input=new String ("bottle mani");
+		String rev="";
+		//char[] chararray=input.toCharArray();
+		
+		
+		 for(int i=input.length()-1; i>=0; i--) {
+			 
+		 
+		 System.out.println(input.charAt(i));
+		 rev=rev+input.charAt(i);
+		
+		 }
+		 if(input.equals(rev)) {
+			 System.out.println(rev+"this is pallindrom" );
+		 }
+		 else {
+			 System.err.println(rev+"this is not pallindrom");
+		 }
+		
+		//	for (int i =name.length()-1; i>=0; i--)
+		
+		
+	
+
+}}
